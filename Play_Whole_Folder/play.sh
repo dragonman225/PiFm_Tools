2f61597645642856c2d3bac547db4cd817d981b8
dragonman225 <zxcvb22217@gmail.com>
2017-01-24 03:26:38 +0800

    Change script to general form.
    Assume ffmpeg and PiFm are in /usr/bin

--- Play_Whole_Folder/play.sh
+++ Play_Whole_Folder/play.sh
@@ -1,10 +1,10 @@
-#!bin/bash
+#!/bin/bash
 echo "Welcome ! Your Bash version ${BASH_VERSION}..."
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
 for i in *
         do
                 echo "Playing $i"
-                /home/pi/Downloads/pifmradio/ffmpeg/arm/bin/ffmpeg -i $i -f s16le -acodec pcm_s16le -ac 2 -ar 44100 - | sudo ~/Downloads/pifmradio/pifm - 87.6 44100 stereo
+                ffmpeg -i $i -f s16le -acodec pcm_s16le -ac 2 -ar 44100 - | sudo pifm - 87.6 44100 stereo
         done
 IFS=$SAVEIFS
